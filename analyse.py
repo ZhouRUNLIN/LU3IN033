235bc2ecdd5903f8ab75255f1517b2ca070289e5
mprTest1 <MA_PEIRAN@OUTLOOK.COM>
2022-12-01 12:13:43 +0100

    0

--- analyse.py
+++ analyse.py
@@ -91,22 +91,83 @@ def ip_version_IHL(s:str):
     l,sr=discharge(s,1)
     version=h2d_half(l[0])
     ihl=h2d_half(l[1])*4
-    return {"Version":version,"IHL":ihl}.update(ip_TOS(sr))
+    return {"Version":version,"IHL":ihl}.update(ip_TOS(sr,ihl-1))
 
-def ip_TOS(s:str):
+def ip_TOS(s:str,IHL:int):
     """
     TOS : 1 octet
     Type of service, le type de service à appliquer au paquet en fonction de certains paramètres comme le délai de transit, la sécurité. Il est peu utilisé et sa valeur est généralement égale à 0.
     """
     l,sr=discharge(s,1)
     tos=l[0]
-    return {"TOS":tos}.update(ip_totalL(sr))
+    return {"TOS":tos}.update(ip_totalL(sr,IHL-1))
 
-def ip_totalL(s:str):
+def ip_totalL(s:str,IHL:int):
     """
     Total length : 2 octets
     La longueur totale du datagramme, exprimée en octets. En pratique, il est rare qu'un datagramme IP fasse plus de 1500 octets
     """
     l,sr=discharge(s,1)
     totalL=h2d_byte(l[0])*256+h2d_byte(l[1])
-    return {"Total length":totalL}.update(ip_identification(sr))
+    return {"Total length":totalL}.update(ip_identification(sr,IHL-2,totalL-4))
+
+def ip_identification(s:str,IHL:int,totalL:int):
+    """
+    Identification : 2 octets
+    Sert en cas de fragmentation/réassemblage du datagramme. Ce champ permet alors à l'entité réceptrice de reconnaître les fragments issus d'un même datagramme initial et qui doivent donc faire l'objet d'un réassemblage.
+    """
+    l,sr=discharge(s,2)
+    idf=l[0]+l[1]
+    return {"Identification":"0x"+idf}.update(ip_flags_fo(sr,IHL-2,totalL-2))
+
+def ip_flags_fo(s:str,IHL:int,totalL:int):
+    """
+    flags : 3 bits
+        bit réservé : mis à 0
+        DF (Don't Fragment) : mis à 1 par l'émetteur pour interdire la fragmentation
+        MF (More Fragment) : mis à 1 pour signifier que le fragment courant est suivi d'un autre fragment
+    fragment offset : 13 bits
+    Donner la position relative du fragment dans le datagramme initial, le déplacement étant donné en unités de 64 bits
+    """
+    l,sr=discharge(s,2)
+    num=int(l[0]+l[1],16)
+    numBin=bin(num)[2:]
+    assert numBin[0]==0
+    if numBin[1]=='1':
+        f="DF"
+    elif numBin[2]=='1':
+        f="MF"
+    else:
+        f="None"
+    numBin=numBin[3:]
+    fo=int(numBin,2)
+    return {"flags":f,"Fragment offset":fo}.update(ip_TTL(sr,IHL-2,totalL-2))
+
+def ip_TTL(s:str,IHL:int,totalL:int):
+    """
+    Identification : 1 octet
+    Time To Live, donner une indication de la limite supérieure du temps de vie d'un datagramme
+    """
+    l,sr=discharge(s,1)
+    ttl=h2d_byte(l[0])
+    return {"TTL":ttl}.update(ip_protocol(sr,IHL-1,totalL-1))
+
+def ip_protocol(s:str,IHL:int,totalL:int):
+    """
+    Identification : 1 octet
+    Le protocole (de niveau supérieur) utilisé pour le champ de données du datagramme.
+    """
+    l,sr=discharge(s,1)
+    protocol=h2d_byte(l[0])
+    assert protocol in (1,6,17)
+    prot_Name={1:"ICMP",6:"TCP",17:"UDP"}[protocol]
+    return {"protocol":prot_Name}.update(ip_header_checksum(sr,IHL-1,totalL-1,prot_Name))
+
+def ip_header_checksum(s:str,IHL:int,totalL:int,protocol:str):
+    """
+    Header checksum : 2 octets
+    Une zone de contrôle d'erreur portant uniquement sur l'en-tête du datagramme
+    """
+    l,sr=discharge(s,2)
+    cs=l[0]+l[1]
+    return {"Header checksum":"0x"+cs}.update(ip_src_addr(sr,IHL-2,totalL-2,protocol))
