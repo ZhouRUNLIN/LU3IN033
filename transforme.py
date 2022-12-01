f013943ecb93608045b053b17383549122850032
RUNLIN ZHOU <72131094+ZhouRUNLIN@users.noreply.github.com>
2022-12-01 13:27:19 +0100

    Add files via upload

--- transforme.py
+++ transforme.py
@@ -1,8 +1,7 @@
 import struct
 import datetime
  
-fpcap = open('trace/trace.pcap', 'rb')
-ftxt = open('trace/trace.txt', 'w')
+fpcap = open('trame/trace.pcap', 'rb')
 string_data = fpcap.read()
 packet_num = 0
 packet_data = []
@@ -25,7 +24,8 @@ def bytes_to_hex(a):
         return hex(a)[2:]
  
 for i in range(packet_num):
-    ftxt.write('Num trame'+str(i+1) +':\n')
+    ficName = 'trame/trame' + str(i) + '.txt'
+    ftxt = open(ficName, 'w')
 
     for j in range(0, len(packet_data[i])):
         ftxt.write(bytes_to_hex(packet_data[i][j])+" ")
