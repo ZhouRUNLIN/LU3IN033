39059b26a76001bb5b3dd831dd3de28d63d8b1b5
RUNLIN ZHOU <72131094+ZhouRUNLIN@users.noreply.github.com>
2022-12-05 00:52:02 +0100

    Add files via upload

--- classifieur.py
+++ classifieur.py
@@ -4,17 +4,7 @@ from analyse import *
 path = "trame"
 files= os.listdir(path) 
 destinationListe = []              #Le classificateur va classer en fonction de l'adresse cible.
-
-for count in range(0,25):
-    s="trame/trame"+str(count)+".txt"
-    f=open(s)
-    iter_f = iter(f)           #la création d'itération
-    str0=""                     #stocker la contenue dans le ficher
-    for line in iter_f:
-        str0+=line
-    print(decode_no_CRC(str0))
     
-"""
 for file in files:                 #parcours répertoire
     if not os.path.isdir(file):    #Déterminez s'il s'agit d'un dossier, ouvrez-le uniquement s'il ne l'est pas.
         f = open(path+"/"+file) 
@@ -22,7 +12,34 @@ for file in files:                 #parcours répertoire
         str=""                     #stocker la contenue dans le ficher
         for line in iter_f:
             str+=line
-        print(decode_no_CRC(str))
-        #dic=decode_no_CRC(str)
-        #print(dic['Eth Destination address'])     
-"""
+        dic=decode_no_CRC(str)
+        if dic['Type']=='IP' and dic['IP protocol']=='TCP' and (dic['IP Destination address'] == '192.168.1.45' or dic['IP Source address'] == '192.168.1.45'):
+            if dic['IP Destination address'] != '192.168.1.45' and dic['IP Destination address'] not in destinationListe:  
+                destinationListe.append(dic['IP Destination address'])
+            if dic['IP Source address'] != '192.168.1.45' and dic['IP Source address'] not in destinationListe:
+                destinationListe.append(dic['IP Source address'])
+        else:
+            os.remove(path+"/"+file)
+
+print(set(destinationListe))
+
+path = "trame"
+files= os.listdir(path) 
+dicVide={}
+for i in range(len(destinationListe)):
+    listeFile=[]
+    for file in files:                 
+        if not os.path.isdir(file):    
+            f = open(path+"/"+file) 
+            iter_f = iter(f)          
+            str=""                     
+            for line in iter_f:
+                str+=line
+            dic=decode_no_CRC(str)
+            if dic['IP Destination address']==destinationListe[i] or dic['IP Source address']==destinationListe[i]:
+                listeFile.append(path+"/"+file)
+    dicVide[destinationListe[i]]=listeFile
+
+print(len(dicVide[destinationListe[0]]))
+print(len(dicVide[destinationListe[1]]))
+print(len(dicVide[destinationListe[2]]))
--- makefile
+++ makefile
@@ -1,8 +1,9 @@
 #capture : capture tous les trames passer sur en0 dans 3 secondes et stocker les ficher en format de .txt
 capture:
-	tcpdump -i en0 -w trame/trace.pcap & sleep 3 
+	tcpdump -i en0 tcp -w trame/trace.pcap & sleep 5 
 	python3 transforme.py
+	rm -f trame/trace.pcap
 
 #clean : initialiser la répertoire
 clean: 
-	rm -f trame/*.pcap trame/*.txt
+	rm -f trame/*.txt
